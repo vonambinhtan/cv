e694d5d6e2ad63e0e8a580213987919e7a345f1d
Hien Ho <hothaohien96@gmail.com>
2020-02-10 18:08:29 +0700

    Updates

---
+++ precache-manifest.2060be2f675fcbdeb8eb7c6d75e9c20e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8cb88229cdc803f81dd1c2a1c7f615c",
+    "url": "/cv/index.html"
+  },
+  {
+    "revision": "1e855f215378dd81d9bd",
+    "url": "/cv/static/css/main.0454bd88.chunk.css"
+  },
+  {
+    "revision": "39372b131cda886cfac5",
+    "url": "/cv/static/js/2.39134b5a.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/cv/static/js/2.39134b5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e855f215378dd81d9bd",
+    "url": "/cv/static/js/main.be8663d5.chunk.js"
+  },
+  {
+    "revision": "9cc8135adf80447f509a",
+    "url": "/cv/static/js/runtime-main.7826774c.js"
+  },
+  {
+    "revision": "658106cd03eb1b743ff76e6f67e824c9",
+    "url": "/cv/static/media/vonambinhtan.658106cd.jpg"
+  }
+]);
